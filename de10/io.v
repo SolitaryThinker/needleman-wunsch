5d48c890a992cb0857cba3a8e4449603a0fe95e9
William Lin <wlsaidhi@gmail.com>
2019-11-18 11:19:23 -0600

    io.v

--- de10/io.v
+++ de10/io.v
@@ -1,14 +1,13 @@
-integer s = $fopen("in.fifo");
+integer s = $fopen("in.fifo", "r");
 reg[31:0] x = 0;
 
 always @(posedge clock.val) begin
-  //$fread(s, x);
+  $fread(s, x);
   $display("what");
   $display(x);
-  $finish;
   if ($feof(s)) begin
     $finish;
   end
-  $display(x);
+  //$display(x);
 end
 
